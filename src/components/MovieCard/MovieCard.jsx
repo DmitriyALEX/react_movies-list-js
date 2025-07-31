294aa9c31c53b43b109208e250dd4f114a0c8927
DmitriyALEX <doiroffical@gmail.com>
2025-07-31 18:31:07 +0300

    updated imports

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,12 +1,14 @@
 import './MovieCard.scss';
 
-export const MovieCard = ({ card }) => {
+export const MovieCard = ({ movie }) => {
+  const { imgUrl, title, description, imdbUrl } = movie;
+
   return (
     <div className="card" data-cy="Movie">
       <div className="card-image">
         <figure className="image is-4by3">
-          {card?.imgUrl ? (
-            <img data-cy="MovieImage" src={card?.imgUrl} alt="Film logo" />
+          {imgUrl ? (
+            <img data-cy="MovieImage" src={imgUrl} alt="Film logo" />
           ) : (
             ''
           )}
@@ -23,15 +25,15 @@ export const MovieCard = ({ card }) => {
 
           <div className="media-content">
             <p className="title is-8" data-cy="MovieTitle">
-              {card.title}
+              {title}
             </p>
           </div>
         </div>
 
         <div className="content">
-          <p data-cy="MovieDescription">{card.description}</p>
+          <p data-cy="MovieDescription">{description}</p>
 
-          <a href={card.imdbUrl} data-cy="MovieLink">
+          <a href={imdbUrl} data-cy="MovieLink">
             IMDB
           </a>
         </div>
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,12 @@
-import './MovieList.scss';
-import { MovieCard } from '../MovieCard/MovieCard';
+import './MovieList.scss'
+import { MovieCard } from '../MovieCard/MovieCard'
 
 export const MovieList = ({ movies }) => {
   return (
     <div className="movies">
-      {movies.map(card => (
-        <MovieCard card={card} key={card.imdbId} />
+      {movies.map((movie) => (
+        <MovieCard movie={movie} key={movie.imdbId} />
       ))}
     </div>
-  );
-};
+  )
+}
